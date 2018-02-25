6f671ce3f76420d8f22f3bf0c72d3e39e9d4c513
Valerie <xwang145@u.rochester.edu>
2018-02-25 00:22:20 -0500

    fixed bugs

--- parserRD.h
+++ parserRD.h
@@ -11,13 +11,13 @@ extern RDP RDP_new(char* input, int length);
 extern void RDP_free(RDP rdp);
 extern bool lookAhead(char c);
 extern bool matchTerminal(char x);
-extern Tree E(RDP rdp);
-extern Tree TT(RDP rdp);
-extern Tree T(RDP rdp);
-extern Tree FT(RDP rdp);
-extern Tree F(RDP rdp);
-extern Tree N(RDP rdp);
-extern Tree NT(RDP rdp);
-extern Tree D(RDP rdp);
+extern Tree E();
+extern Tree TT();
+extern Tree T();
+extern Tree FT();
+extern Tree F();
+extern Tree N();
+extern Tree NT();
+extern Tree D();
 
 #endif
