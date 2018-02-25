4cc8f54b8851a8b37853fbc876bead5fb64fe5f4
Valerie <xwang145@u.rochester.edu>
2018-02-25 17:36:13 -0500

    updated calcualtor

--- calculator.c
+++ calculator.c
@@ -1,5 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 #include "calculator.h"
+#include "parseTree.h"
+#include "parserRD.h"
 
 bool isOperand(char c) {
 	return (c >= '0' && c <= '9');	
@@ -17,9 +22,17 @@ bool isOperator(char c) {
 	return false;
 }
 
-int evaluation_tree(Tree tree) {
+double combine (double a, double b) {
 	char exp[128];
-	int result = 0;
+	double n;
+	sprintf(exp, "%d %d", double a, double b);
+	sscanf(exp, "%d", &n);
+	return n;
+}
+
+double evaluation_tree(Tree tree) {
+	// char exp[128];
+	double result = 0;
 	if (tree->data == 'E') {
 		return evaluation_tree(tree->leftmostChild);
 	}
@@ -36,13 +49,15 @@ int evaluation_tree(Tree tree) {
 
 	} 
 	else if (tree->data == 'N') {
-
+		return evaluation_tree(tree->leftmostChild);
 	}
 	else if (tree->data == 'n') {
-
+		return evaluation_tree(tree->leftmostChild);
 	}
 	else if (tree->data == 'D') {
-
+		if (tree->rightSibling->leftmostChild->data == 'e')
+			return evaluation_tree(tree->leftmostChild);
+		return combine(evaluation_tree(tree->leftmostChild, tree->rightSibling));
 	}
 	else if (isOperand(tree->data)) {
 		int n = tree->data;
@@ -51,7 +66,7 @@ int evaluation_tree(Tree tree) {
 	else if (isOperator(tree->data)) {
 
 	}
-	
+	return result;
 	
 }
 
