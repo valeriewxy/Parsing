31f8749f4e8d30199524af2a22bfe2cbcbe6e6ff
Valerie <xwang145@u.rochester.edu>
2018-02-25 23:28:57 -0500

    tdp started

---
+++ parserTD.h
@@ -0,0 +1,27 @@
+#ifndef _parsertd_h
+#define _parsertd_h
+
+#include <stdbool.h>
+#include "parseTree.h"
+
+typedef struct TDParser* TDP;
+
+struct TDParser {
+	int parsingTable**;
+	Tree tree;
+	int current;
+	int length;	
+};
+
+extern Tree* stack;
+extern void push(Tree tree);
+extern Tree peek();
+extern Tree pop();
+
+extern TDP TDP_new(char* input, int length);
+extern void TDP_free(TDP rdp);
+extern bool TD_lookAhead(TDP rdp, char c);
+extern bool TD_matchTerminal(TDP rdp, char x);
+
+
+#endif
